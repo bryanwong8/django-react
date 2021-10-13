b7a8ea8426d84ef24eb8e3df285ecf5a87367813
Bryan Wong <bryanwong7788@gmail.com>
2021-10-13 15:20:04 -0700

    Setup API for backend

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    completed = models.BooleanField(default=False)
+
+    # This method just tells Django what to print when it needs to print out an instance of the any model
+    def _str_(self):
+        return self.title
