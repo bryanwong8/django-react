3449cc20cdc3799b16880e4ff2b94c8f42f246aa
Bryan Wong <bryanwong7788@gmail.com>
2021-10-15 15:38:32 -0700

    Revert "Added new fields to Todo model"
    This reverts commit af95c9660e74ddb93bf5a11e485a3c30cea76ea6.

--- backend/todo/admin.py
+++ backend/todo/admin.py
@@ -1,10 +1,8 @@
 from django.contrib import admin
 from .models import Todo
 
-
 class TodoAdmin(admin.ModelAdmin):
-    list_display = ("title", "description", "completed", "priority", "due_date",)
-
+    list_display = ('title', 'description', 'completed')
 
 # Register your models here.
 admin.site.register(Todo, TodoAdmin)
