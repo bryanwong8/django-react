2c6ebdd66860ee73b07aa6efe6803dd99d3b4c60
Bryan Wong <bryanwong7788@gmail.com>
2021-10-18 10:50:21 -0700

    Added priority field to database

--- backend/todo/models.py
+++ backend/todo/models.py
@@ -1,9 +1,21 @@
 from django.db import models
 
+LOW = "LOW"
+MID = "MID"
+HIGH = "HIGH"
+
+MONTH_CHOICES = (
+    (LOW, "Low"),
+    (MID, "Mid"),
+    (HIGH, "High"),
+)
+
+
 class Todo(models.Model):
     title = models.CharField(max_length=120)
     description = models.TextField()
     completed = models.BooleanField(default=False)
+    priority = models.CharField(max_length=4, choices=MONTH_CHOICES, default=LOW)
 
     # This method just tells Django what to print when it needs to print out an instance of the any model
     def _str_(self):
