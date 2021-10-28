6623b1636cdba27e8cc0b5a2d90603a3b2640187
Bryan Wong <bryanwong7788@gmail.com>
2021-10-28 15:24:41 -0700

    Updated frontend to use customStore since pagination returns a different payload back

--- frontend/src/components/TableItems.tsx
+++ frontend/src/components/TableItems.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { TodoItem } from "../shared/types/Todo";
 import { Button, Column, DataGrid, FilterRow, Paging, Pager, RemoteOperations, } from 'devextreme-react/data-grid';
-import { createStore } from 'devextreme-aspnet-data-nojquery';
+import CustomStore from 'devextreme/data/custom_store';
 
 interface TableItemsProps {
   todoList: TodoItem[],
@@ -13,9 +13,48 @@ interface TableItemsProps {
 const TableItems = (props: TableItemsProps) => {
   const { viewCompleted } = props;
   const displayMode = { text: 'Display Mode \'full\'', value: 'full' };
-  const dataSource = createStore({
+
+  // Function to see if a param exists or not. 
+  // Unfortunately the value can be a number, boolean string, null or an array of varying types
+  const isNotEmpty = (value: any) => {
+    return value !== undefined && value !== null && value !== '';
+  }
+
+  
+  const dataSource = new CustomStore({
     key: 'id',
-    loadUrl: `/api/todos?completed=${viewCompleted}`,
+    load: async (loadOptions: any) => {
+      let params = '&';
+      const paramsList = [
+        'skip',
+        'take',
+        'requireTotalCount',
+        'requireGroupCount',
+        'sort',
+        'filter',
+        'totalSummary',
+        'group',
+        'groupSummary'
+      ]
+
+      // Grabs query params from loadOption and strings it together to send to the API
+      paramsList.forEach((param: string) => {
+        if (param in loadOptions && isNotEmpty(loadOptions[param])) {
+          params += `${param}=${JSON.stringify(loadOptions[param])}&`;
+        }
+      });
+
+      // This removes the trailing & from the string
+      params = params.slice(0, -1);
+
+      const response = await fetch(`/api/todos?completed=${viewCompleted}${params}`);
+      const data = await response.json();
+
+      return {
+        data: data.results,
+        totalCount: data.count,
+      }
+    }
   });
 
   return (
